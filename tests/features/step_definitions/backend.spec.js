afaefba2edb4ed8532627cca5d9f45b2c0e7f567
Ioan Ghisoi <ionutghisoi95@gmail.com>
2017-12-13 16:37:39 +0000

    remove yet another duplicated step

--- tests/features/step_definitions/backend.spec.js
+++ tests/features/step_definitions/backend.spec.js
@@ -7,45 +7,6 @@ const BACKEND = Globals.selector.backend;
 const FRONTEND = Globals.selector.frontend;
 
 export default function () {
-  this.Given(/^I disable the url secret key encryption$/, () => {
-    browser.url(URL.magento_base + URL.admin_path);
-    if (browser.isVisible(BACKEND.admin_username)) {
-      browser.setValue(BACKEND.admin_username, VAL.admin.username);
-      browser.setValue(BACKEND.admin_password, VAL.admin.password);
-      browser.click(BACKEND.admin_sign_in);
-    }
-    browser.url(URL.magento_base + URL.admin_path); // avoid magento cache warning
-    browser.waitUntil(function () {
-      return browser.isVisible(BACKEND.stores);
-    }, VAL.timeout_out, 'stores button should be visible');
-    browser.click(BACKEND.stores);
-    browser.waitUntil(function () {
-      return browser.isVisible(BACKEND.configuration);
-    }, VAL.timeout_out, 'configuration button should be visible');
-    browser.click(BACKEND.configuration);
-    browser.waitUntil(function () {
-      return browser.isVisible(BACKEND.advanced);
-    }, VAL.timeout_out, 'advanced button should be visible');
-    browser.click(BACKEND.advanced);
-    browser.waitUntil(function () {
-      return browser.isVisible(BACKEND.admin);
-    }, VAL.timeout_out, 'admin button should be visible');
-    browser.click(BACKEND.admin);
-    browser.waitUntil(function () {
-      return browser.isVisible(BACKEND.admin_security);
-    }, VAL.timeout_out, 'admin security button should be visible');
-    browser.click(BACKEND.admin_security);
-    browser.waitUntil(function () {
-      return browser.isVisible(BACKEND.admin_security_key);
-    }, VAL.timeout_out, 'admin security key option button should be visible');
-    browser.click(BACKEND.admin_security_key);
-    browser.waitUntil(function () {
-      return browser.isVisible(BACKEND.security_key_option);
-    }, VAL.timeout_out, 'admin security option button should be visible');
-    browser.selectByValue(BACKEND.security_key_option, '0');
-    browser.click(BACKEND.plugin.save);
-  });
-
   this.Given(/^I set the integration type to (.*)$/, (integration) => {
     switch (integration) {
       case 'frames':
