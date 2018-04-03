0d4bf66699d62b46c90866a74a3b14af70adec04
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2018-04-03 19:00:43 +0100

    Add wait

--- tests/features/step_definitions/payment.spec.js
+++ tests/features/step_definitions/payment.spec.js
@@ -21,7 +21,9 @@ export default function () {
       return browser.isVisible(FRONTEND.order.checkout_iframe_selector);
     }, VAL.timeout_out, 'the embedded form should be visible');
     const iframe = browser.element(FRONTEND.order.checkout_iframe_selector);
+    browser.pause(3000);
     browser.frame(iframe.value);
+    browser.pause(3000);
     switch (option) {
       case 'visa':
         card = browser.element(FRONTEND.order.embedded_fields.card_number);
